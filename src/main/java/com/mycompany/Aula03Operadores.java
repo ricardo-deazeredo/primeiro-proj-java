03a0df2df0be38fd8ef0e21dfec994b13ad654c8
ricardo-deazeredo <ricardo.azeredoaz@gmail.com>
2025-04-02 19:40:10 +0000

    aula03 quase acabada

---
+++ src/main/java/com/mycompany/Aula03Operadores.java
@@ -0,0 +1,19 @@
+package com.mycompany;
+
+public class Aula03Operadores {
+
+    public static void main(String[] args) {
+        
+        /**
+         * Tipos de comentário
+         */
+
+         // Comentário de uma linha
+
+         /**
+          * Comentário de
+          * multiplas
+          * linhas
+          */
+    }
+}
