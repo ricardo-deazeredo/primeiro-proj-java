d2f06f73d8840e6b08eae99e9165bb863e80e717
ricardo-deazeredo <ricardo.azeredoaz@gmail.com>
2025-04-08 11:59:52 +0000

    declaração de variáveis feita

---
+++ src/main/java/com/mycompany/avaliação.java
@@ -0,0 +1,12 @@
+package com.mycompany;
+
+public class avaliação {
+        /**
+         * Declaração de variáveis
+         */
+
+         String var1; // Toda variável declarada sem valor, automaticamente é null,
+         String var2 = null; // Variável declarada com valor null
+         String var3 = ""; // Variável declarada com valor vazio (diferente de null)
+ 
+}
