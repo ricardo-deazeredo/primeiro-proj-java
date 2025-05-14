f8eec8808519434d9b1f7bf1467f497850d5d86c
ricardo-deazeredo <ricardo.azeredoaz@gmail.com>
2025-05-14 19:36:03 +0000

    exercício concluído

---
+++ src/main/java/com/mycompany/Ex03.java
@@ -0,0 +1,31 @@
+package com.mycompany;
+
+import java.util.Scanner;
+
+public class Ex03 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Digite seu IMC: ");
+
+        double imc = scan.nextDouble();
+
+        if (imc < 16.0) {
+            System.out.println("Magreza grau III");
+        } else if (imc >= 16.0 && imc <= 16.9) {
+            System.out.println("Magreza grau II");
+        } else if (imc >= 17.0 && imc <= 18.4) {
+            System.out.println("Magreza grau I");
+        } else if (imc >= 18.5 && imc <= 24.9) {
+            System.out.println("Eutrofia");
+        } else if (imc >= 25.0 && imc <= 29.9) {
+            System.out.println("Pré-obesidade");
+        } else if (imc >= 30.0 && imc <= 34.9) {
+            System.out.println("Obesidade moderada (grau I)");
+        } else if (imc >= 35.0 && imc <= 39.9) {
+            System.out.println("Obesidade severa(grau II)");
+        } else {
+            System.out.println("obesidade severa (grau III)");
+        }
+    }
+}
