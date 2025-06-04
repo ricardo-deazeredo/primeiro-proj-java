0713125624546e4c3b74d55375a648b088f8019a
ricardo-deazeredo <ricardo.azeredoaz@gmail.com>
2025-06-04 19:33:27 +0000

    aula 08 funçoes recursivas

--- src/main/java/com/mycompany/Aula08funçoes.java
+++ src/main/java/com/mycompany/Aula08funçoes.java
@@ -10,9 +10,35 @@ public class Aula08funçoes {
         for (int i = 1; i <= 10; i++) {
             System.out.println(numero + " x " + i + " = " + multiplicacao(numero, i));
         }
+        System.out.println(fatorial(numero));
     }
+
+    /**
+     * Não podemos declarar uma função dentro da outra.
+     * 
+     * Toda função deve ter um retorno, quando não há retorno utilizamos void.
+     * 
+     * Uma função pode não ter parâmetros, ou ter um ou mais.
+     */
 
     public static int multiplicacao(int num, int num2) {
         return num * num2;
     }
+
+    /**
+     * Funções recursivas
+     * 
+     * Uma função recursiva faz uma chamada para ela própria porém, toda
+     * recusrividade deve ter um ponto de parada,
+     * caso contrário pode resultar em um loop infinito.
+     */
+
+    public static int fatorial(int n) {
+        if (n != 0) {
+            return n * fatorial(n - 1);
+        } else {
+            return 1;
+        }
+    }
+
 }
