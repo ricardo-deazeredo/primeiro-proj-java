a0129396643d3a5b13c754ede2daa4bdbec02d50
ricardo-deazeredo <ricardo.azeredoaz@gmail.com>
2025-08-29 14:37:38 +0000

    aula 12, construtores

---
+++ src/main/java/com/mycompany/poo/aula12/App.java
@@ -0,0 +1,7 @@
+package com.mycompany.poo.aula12;
+
+public class App {
+    public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa();
+        Pessoa pessoa2 = new Pessoa("Ricardo")
+}
---
+++ src/main/java/com/mycompany/poo/aula12/Pessoa.java
@@ -0,0 +1,65 @@
+package com.mycompany.poo.aula12;
+
+public class Pessoa {
+    /**
+     * Construtores
+     * 
+     * Utilizamos o método construtor para inicializar nossas classes, funcionando com uma configuração 
+     * prévia da mesma.
+     * 
+     * Toda classe quando é criada, possui um método  construtor vazio que é chamado no momento que instanciamos a mesma
+     * 
+     * Uma mesma classe pode conter mais de um construtor,
+     * porém cada um deve possuir quantidade e 
+     * tipos de parâmetros diferentes
+     */
+    private String nome;
+    private int idade;
+    private double altura;
+    private double peso;
+
+    // Método construtor padrão
+    // Todo método comstrutor possui o mesmo nome da classe 
+    public Pessoa(){
+        System.out.println("Chamando o construtor padrão");
+    }
+
+    public Pessoa(String nome){
+        this.nome = nome;
+        System.out.println("Chamando construtor 2");
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+
+}
